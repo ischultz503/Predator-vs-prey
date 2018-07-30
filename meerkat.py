2ccf4a82af518af0ec5f84f1edbd84d679ab724c
Isaac <isaac@Adams-Air.lan>
2018-07-30 16:02:31 -0700

    first commit

---
+++ meerkat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib
+
+def header():
+    print("\n1. If there were no wooks, the population of chads would double every month.\n2. If there were no chads, the popopulation of wooks would halve every month.\n3. If you had exactly 20 chads and 5 wooks, the population would not change at all.\n")
+
+
+if __name__ == "main":
+    
+    print("How many wooks to start?")
+    wooks = input()
+    print("How many chads?")
+    chads = input()
